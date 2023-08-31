b4c89f4742dfac857c21a4d4debbb35b906f9e17
Abisheck26 <abisheckgnanavel@gmail.com>
2023-08-31 12:02:22 +0530

    add config and constants

---
+++ constants/index.go
@@ -0,0 +1,6 @@
+package constants
+
+const ConnectionString = "mongodb+srv://abisheckg:abi123@abisheck.ajm1rc5.mongodb.net/?retryWrites=true&w=majority"
+const Dbname = "netxdb"
+
+const Port = ":4000"
